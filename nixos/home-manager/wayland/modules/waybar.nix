1483e7fb7b4ead821456ae233894a3a10ec5be04
juno-soyr <h.gaspar@proton.me>
2025-10-11 14:56:33 +0200

    waybar update

--- nixos/home-manager/wayland/modules/waybar.nix
+++ nixos/home-manager/wayland/modules/waybar.nix
@@ -3,8 +3,172 @@
   lib,
   pkgs,
   ...
-}:{
-	programs.waybar = {
-		enable = true;
-	};
+}: {
+  programs.waybar = {
+    enable = true;
+    settings = {
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 32;
+          margin-top = 5;
+          margin-bottom = 0;
+          margin-right = 7;
+          margin-left = 7;
+
+          modules-left = ["clock" "niri/workspaces" "mpris"];
+          modules-center = ["privacy" "niri/window" "group/system-metrics"];
+          modules-right = [
+            "battery"
+            "power-profiles-daemon"
+            "pulseaudio"
+            "niri/language"
+            "tray"
+            "custom/notification"
+            "custom/power"
+          ];
+
+          mpris = {
+            format = "{status_icon} {dynamic}";
+            format-playing = "  <b>{title}</b> - {artist} - {position}";
+            format-paused = "󰏤 <b>{title}</b> - {artist} - {position}";
+            title-len = 35;
+            artist-len = 20;
+            dynamic-len = 30;
+            dynamic-order = ["title" "artist" "position"];
+            dynamic-separator = " - ";
+            dynamic-importance-order = ["title" "artist" "position"];
+            ellipsis = "…";
+          };
+
+          "custom/notification" = {
+            format = "";
+            on-click = "swaync-client -t";
+          };
+
+          "custom/power" = {
+            format = "⏻ ";
+            on-click = "eww open --toggle pwrmnu";
+          };
+
+          "niri/language" = {
+            format-ar = " عر ";
+            format-en = " EN ";
+            on-click = "niri msg action switch-layout next";
+          };
+
+          "niri/window" = {
+            format = "   {title} ";
+            rewrite = {
+              "(.*)~ " = "Terminal";
+            };
+          };
+
+          tray = {
+            icon-size = 18;
+            spacing = 20;
+          };
+
+          pulseaudio = {
+            format = "{icon} {volume}%";
+            format-muted = "󰝟 ";
+            format-icons = {
+              default = ["󰕿" "󰖀" "󰕾"];
+              bluetooth = "󰂯";
+            };
+            max-volume = 100;
+            scroll-step = 5;
+            tooltip = true;
+          };
+
+          power-profiles-daemon = {
+            format = " {icon} ";
+            tooltip-format = "Power profile: {profile}\nDriver: {driver}";
+            tooltip = true;
+            format-icons = {
+              performance = "";
+              balanced = " ";
+              power-saver = "󰌪";
+            };
+          };
+
+          battery = {
+            format = "{icon} {capacity}%";
+            format-icons = [
+              "󰂎"
+              "󰁺"
+              "󰁻"
+              "󰁼"
+              "󰁽"
+              "󰁾"
+              "󰁿"
+              "󰂀"
+              "󰂁"
+              "󰂂"
+              "󰁹"
+            ];
+            format-charging = "󰂄 {capacity}%";
+            interval = 30;
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+            tooltip = true;
+          };
+
+          "group/system-metrics" = {
+            orientation = "horizontal";
+            modules = ["cpu" "memory" "temperature" "network"];
+            drawer = {
+              transition-duration = 500;
+              transition-left-to-right = true;
+            };
+          };
+
+          network = {
+            format = " {bandwidthUpBits}  {bandwidthDownBits}";
+            interval = 1;
+          };
+
+          cpu = {
+            format = "  {usage}%";
+            interval = 1;
+          };
+
+          memory = {
+            format = "  {percentage}%";
+            interval = 1;
+          };
+
+          temperature = {
+            format = " {temperatureC} C°";
+            thermal-zone = 9;
+            interval = 1;
+          };
+
+          clock = {
+            on-click = "eww open --toggle calendarwidget";
+            interval = 60;
+            tooltip = true;
+            format = "{:%I : %M %p}";
+            tooltip-format = "{:%Y-%m-%d}";
+          };
+
+          "niri/workspaces" = {
+            persistent-workspaces = {
+              "*" = 5;
+            };
+            active-only = false;
+            all-outputs = true;
+            format = "{icon}";
+            format-icons = {
+              active = "   ";
+              default = "   ";
+            };
+          };
+        };
+      };
+    };
+  };
 }

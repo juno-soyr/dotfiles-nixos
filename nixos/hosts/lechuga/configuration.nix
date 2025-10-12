7974f25d235612e809c32a53c616d74748af5f0e
juno-soyr <h.gaspar@proton.me>
2025-10-12 19:21:45 +0200

    stylix

--- nixos/hosts/lechuga/configuration.nix
+++ nixos/hosts/lechuga/configuration.nix
@@ -104,6 +104,14 @@
       "${config.system.path}"
     ];
   };
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/whoisYoges/lwalpapers/refs/heads/PicturesOnly/wallpapers/b-010.jpg";
+      hash = "sha256-oUXpV05POhAypQHibL+kQUHN0MT6ny/+meH60YLfkjM=";
+    };
+  };
   # Allow unfree package
   nixpkgs.config.allowUnfree = true;
 

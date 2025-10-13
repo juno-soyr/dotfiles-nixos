afc3f25f0c76fd58b4b626c237f7be2751067fbf
soyr <h.gaspar@proton.me>
2025-10-13 15:11:38 +0200

    mako config

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -161,17 +161,16 @@
     };
   };
   services.mako.enable = true;
-  services.mako.settings = ''
-        # Colors
+  services.mako.settings = {
+    background-color = "#1e1e2e";
+    text-color = "#cdd6f4";
+    border-color = "#b4befe";
+    progress-color = over "#313244";
 
-    background-color=#1e1e2e
-    text-color=#cdd6f4
-    border-color=#b4befe
-    progress-color=over #313244
-
-    [urgency=high]
-    border-color=#fab387
-  '';
+    "urgency=high" = {
+      border-color = "#fab387";
+    };
+  };
   programs.bash = {
     enable = true;
     enableCompletion = true;

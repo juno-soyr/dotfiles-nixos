cc3b094003740dbea645d61427700cf2c0e279ff
soyr <h.gaspar@proton.me>
2025-12-10 04:30:39 +0100

    fixing grim screensharing

--- nixos/hosts/lechuga/configuration.nix
+++ nixos/hosts/lechuga/configuration.nix
@@ -5,7 +5,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -24,7 +25,7 @@
 
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
@@ -96,7 +97,7 @@
     packages = with pkgs; [
       wget
       git
-
+      grim
       #   kdePackages.kate
     ];
   };
@@ -104,7 +105,7 @@
   virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.draganddrop = true;
-  users.extraGroups.vboxusers.members = ["soyr"];
+  users.extraGroups.vboxusers.members = [ "soyr" ];
   environment.sessionVariables = {
     # Workaround for cursor theme not being recognized
     XCURSOR_PATH = [
